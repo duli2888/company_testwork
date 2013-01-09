05ef64b2b351f87502c2b119b88b75bde39f0fbd
杜黎 <duli@novel-supertv.com>
2013-01-09 16:55:47 +0800

    对软链接等特殊文件的支持，remove->unlink,fscanf获取文件名

--- linux_sys/file_op/delete_file/main.c
+++ linux_sys/file_op/delete_file/main.c
@@ -7,65 +7,67 @@
 
 #include <errno.h>
 
-#define MAX_NAME 20
+#define MAX_NAME 256
 
 int main()
 {
 	FILE *fp;
 	int ret;
 	char tmp_name[MAX_NAME];
-	int i = 0,  pre_position = 0;
-	int flag = 0;
 
 	struct stat file_state;
 
 	memset(tmp_name, '\0', MAX_NAME);
-	
+
 	if ((fp = fopen("file_list", "r")) == NULL) {
 		printf("Opening the file_list file is fail\n");
 		return 0;
 	}
 
-
-	for (flag = 0; flag < 2; flag ++) {
-		while(1) {
-			ret = fscanf(fp, "%s",tmp_name);
-			if (ret == EOF){
-				break;
-			}
-
-			if (lstat(tmp_name, &file_state) < 0) 
-			{ 
-				if (flag == 1) {
-					goto end;
-				} else {
-					fprintf(stderr,"error in CreateProcess %s",strerror(errno));
-					printf("lstat error for %s\r\n", tmp_name); 
-					return -1; 
-				}
-			}
-
-			if (flag == 1) {  // file delete a derctory
-				if (S_ISDIR(file_state.st_mode)){
-					if (remove(tmp_name))
-						printf("Delete %s FAILE\n", tmp_name);
-					else 
-						printf("Delete %s Ok\n",tmp_name);
-				}
-			} else if(flag == 0) {  // delete a general file
-				if (S_ISREG(file_state.st_mode)){
-					if (remove(tmp_name))
-						printf("Delete %s FAILE\n", tmp_name);
-					else 
-						printf("Delete %s Ok\n",tmp_name);
-				}
-			}
-end:
-			memset(tmp_name, '\0', MAX_NAME);
+	while(1) {
+		ret = fscanf(fp, "%s",tmp_name);
+		if (ret == EOF){
+			break;
 		}
-		fseek(fp, 0, SEEK_SET);
+
+		if (lstat(tmp_name, &file_state) < 0) 
+		{ 
+				fprintf(stderr,"error in CreateProcess %s",strerror(errno));
+				printf("lstat error for %s\r\n", tmp_name); 
+				continue; 
+		}
+
+		if (S_ISDIR(file_state.st_mode) != 1){
+			if (unlink(tmp_name))
+				printf("Delete %s FAILE\n", tmp_name);
+			else 
+				printf("Delete %s Ok\n",tmp_name);
+		}
+		memset(tmp_name, '\0', MAX_NAME);
 	}
 
-	fclose(fp);
-	return 0;
+	fseek(fp, 0, SEEK_SET);
+
+	while(1) {
+		memset(tmp_name, '\0', MAX_NAME);
+		ret = fscanf(fp, "%s",tmp_name);
+		if (ret == EOF){
+			break;
+		}
+
+		if (lstat(tmp_name, &file_state) < 0) 
+		{ 
+			continue;
+		}
+
+		if (S_ISDIR(file_state.st_mode)){
+			if (remove(tmp_name))
+				printf("Delete %s directory\n", tmp_name);
+			else 
+				printf("Delete %s Ok\n",tmp_name);
+		}
+	}
+
+		fclose(fp);
+		return 0;
 }
--- linux_sys/file_op/delete_file/new_file.sh
+++ linux_sys/file_op/delete_file/new_file.sh
@@ -2,6 +2,7 @@
 touch a.txt b.txt c.txt d.txt e.txt f.txt g.txt
 mkdir aa bb cc dd ee ff gg
 mkdir aa/aaa aa/bbb aa/ccc
+ln -s a.txt gg
 touch aa/a.txt aa/aaa/a.txt
 mkdir bb/aaa bb/bbb bb/ccc
 touch bb/a.txt bb/aaa/a.txt
